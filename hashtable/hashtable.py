fa19f6c42ff70f19dc955aaae37d1373c851197c
@gmgower <gmgower@gmail.com>
2020-07-08 21:05:00 -0500

    Implement HashTable capacity constructor.

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -28,6 +28,8 @@ class HashTable:
 
     def __init__(self, capacity):
         # Your code here
+        self.capacity = capacity
+        self.data = [None] * capacity
 
 
     def get_num_slots(self):
@@ -41,7 +43,7 @@ class HashTable:
         Implement this.
         """
         # Your code here
-
+        return self.capacity
 
     def get_load_factor(self):
         """
