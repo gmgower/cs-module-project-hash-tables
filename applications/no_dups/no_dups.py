1b4ed1de6b04dbf26c149b10ace4eaf7178215bd
@gmgower <gmgower@gmail.com>
2020-07-16 20:57:41 -0500

    no_dups

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,8 +1,13 @@
 def no_dups(s):
     # Your code here
-
-
-
+    words = s.strip().split()
+    new = ""
+    duplicate_dic = {}
+    for word in words:
+        if word not in duplicate_dic.keys():
+            duplicate_dic[word] = 1
+            new += word + ' '
+    return new.strip()
 if __name__ == "__main__":
     print(no_dups(""))
     print(no_dups("hello"))
